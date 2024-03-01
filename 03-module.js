ce68b035e10598360d837994d96f6ed3278d85b0
Segoy <segoy.thiel@protonmail.com>
2024-03-01 12:30:41 +0100

    filesystem sync and async tutorial

---
+++ 03-module.js
@@ -0,0 +1,11 @@
+const names = require('./04-names');
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-export')
+require('./07-mind-grenade')
+
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
+
+
