f520dbf8e3c47ce02a6e853ae558a82027c33a1f
Segoy <segoy.thiel@protonmail.com>
2024-03-01 15:10:16 +0100

    Await/Promises Tutorial

--- 2-async-patterns/2-await-pattern.js
+++ 2-async-patterns/2-await-pattern.js
@@ -3,21 +3,17 @@ const {readFile,writeFile} = require('fs');
 // const readFilePromise = util.promisify(readFile)
 // const writeFilePromise = util.promisify(writeFile)
 
-
-
-
 const start = async () =>{
     try{
-    const  first = await readFile('./content/first.txt', 'utf8');
-    const  second = await readFile('./content/second.txt', 'utf8');
-    await writeFile('./content/result-mind-grenade.txt', `This is the result: ${first}, ${second}`)
-    console.log(first, second)
+        const  first = await readFile('./content/first.txt', 'utf8');
+        const  second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt', `This is the result: ${first}, ${second}`)
+        console.log(first, second)
     }catch (error){
         console.log(error)
     }
 }
 start()
-
 
 // getText('./content/first.txt')
 //     .then((result) => console.log(result))
